55cefe3cb952005f88a6385a1f712815918d83a6
Emilio Armas <emilio.armas@epn.edu.ec>
2025-05-16 15:26:24 -0500

    Added login activity

---
+++ app/src/main/java/com/emilio/armas/cazarpatos/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.emilio.armas.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+const val TAG = "Login Activity"
+class LoginActivity : AppCompatActivity() {
+    private lateinit var buttonLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+        }
+    }
+}
